1d8629b82ef3eff7b899af4413347c6c6e93716f
unknown <haiduc0147@gmail.com>
2022-11-18 21:45:25 +0700

    edit migration

--- midterm.sql
+++ midterm.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.3
--- Dumped by pg_dump version 14.3
+-- Dumped from database version 14.1
+-- Dumped by pg_dump version 14.1
 
--- Started on 2022-11-14 23:35:03
+-- Started on 2022-11-18 21:42:08
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,44 +23,30 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 210 (class 1259 OID 33263)
--- Name: Account; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 210 (class 1259 OID 28161)
+-- Name: account; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Account" (
-    "ID" integer NOT NULL,
-    "Username" character varying(50) NOT NULL,
-    "Email" character varying(100) NOT NULL,
-    "Password" character varying(100),
-    "Role" integer NOT NULL,
-    "External_ID" character varying(50),
-    "isActivated" integer NOT NULL
+CREATE TABLE public.account (
+    id integer NOT NULL,
+    username character varying(100),
+    password character varying(100),
+    email character varying(100),
+    external_id character varying(100),
+    role integer NOT NULL,
+    is_activated boolean
 );
 
 
-ALTER TABLE public."Account" OWNER TO postgres;
+ALTER TABLE public.account OWNER TO postgres;
 
 --
--- TOC entry 214 (class 1259 OID 33286)
--- Name: Account_Group; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 209 (class 1259 OID 28160)
+-- Name: Account_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Account_Group" (
-    "ID" integer NOT NULL,
-    "GroupID" integer NOT NULL,
-    "AccountID" integer NOT NULL
-);
-
-
-ALTER TABLE public."Account_Group" OWNER TO postgres;
-
---
--- TOC entry 213 (class 1259 OID 33285)
--- Name: Account_Group_ID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public."Account_Group" ALTER COLUMN "ID" ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public."Account_Group_ID_seq"
+ALTER TABLE public.account ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Account_id_seq"
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -70,12 +56,26 @@ ALTER TABLE public."Account_Group" ALTER COLUMN "ID" ADD GENERATED ALWAYS AS IDE
 
 
 --
--- TOC entry 209 (class 1259 OID 33262)
--- Name: Account_ID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- TOC entry 213 (class 1259 OID 28172)
+-- Name: account_group; Type: TABLE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public."Account" ALTER COLUMN "ID" ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public."Account_ID_seq"
+CREATE TABLE public.account_group (
+    id integer NOT NULL,
+    group_id integer NOT NULL,
+    account_id integer NOT NULL
+);
+
+
+ALTER TABLE public.account_group OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 28175)
+-- Name: account_group_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.account_group ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.account_group_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -85,27 +85,27 @@ ALTER TABLE public."Account" ALTER COLUMN "ID" ADD GENERATED ALWAYS AS IDENTITY 
 
 
 --
--- TOC entry 212 (class 1259 OID 33269)
--- Name: Group; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 212 (class 1259 OID 28167)
+-- Name: group; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Group" (
-    "ID" integer NOT NULL,
-    "Name" character varying(100) NOT NULL,
-    "Description" character varying(100),
-    "InvitationLink" character varying(200) NOT NULL
+CREATE TABLE public."group" (
+    id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    description character varying(100),
+    invitation_link character varying(200) NOT NULL
 );
 
 
-ALTER TABLE public."Group" OWNER TO postgres;
+ALTER TABLE public."group" OWNER TO postgres;
 
 --
--- TOC entry 211 (class 1259 OID 33268)
--- Name: Group_ID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- TOC entry 211 (class 1259 OID 28166)
+-- Name: group_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public."Group" ALTER COLUMN "ID" ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public."Group_ID_seq"
+ALTER TABLE public."group" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.group_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -115,110 +115,103 @@ ALTER TABLE public."Group" ALTER COLUMN "ID" ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 3322 (class 0 OID 33263)
+-- TOC entry 3322 (class 0 OID 28161)
 -- Dependencies: 210
--- Data for Name: Account; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: account; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Account" ("ID", "Username", "Email", "Password", "Role", "External_ID", "isActivated") FROM stdin;
-\.
 
 
 --
--- TOC entry 3326 (class 0 OID 33286)
--- Dependencies: 214
--- Data for Name: Account_Group; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 3325 (class 0 OID 28172)
+-- Dependencies: 213
+-- Data for Name: account_group; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Account_Group" ("ID", "GroupID", "AccountID") FROM stdin;
-\.
 
 
 --
--- TOC entry 3324 (class 0 OID 33269)
+-- TOC entry 3324 (class 0 OID 28167)
 -- Dependencies: 212
--- Data for Name: Group; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: group; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Group" ("ID", "Name", "Description", "InvitationLink") FROM stdin;
-\.
 
 
 --
 -- TOC entry 3332 (class 0 OID 0)
--- Dependencies: 213
--- Name: Account_Group_ID_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Dependencies: 209
+-- Name: Account_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Account_Group_ID_seq"', 1, false);
+SELECT pg_catalog.setval('public."Account_id_seq"', 1, false);
 
 
 --
 -- TOC entry 3333 (class 0 OID 0)
--- Dependencies: 209
--- Name: Account_ID_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Dependencies: 214
+-- Name: account_group_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Account_ID_seq"', 1, false);
+SELECT pg_catalog.setval('public.account_group_id_seq', 1, false);
 
 
 --
 -- TOC entry 3334 (class 0 OID 0)
 -- Dependencies: 211
--- Name: Group_ID_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: group_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Group_ID_seq"', 1, false);
-
-
---
--- TOC entry 3179 (class 2606 OID 33290)
--- Name: Account_Group Account_Group_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."Account_Group"
-    ADD CONSTRAINT "Account_Group_pkey" PRIMARY KEY ("GroupID");
+SELECT pg_catalog.setval('public.group_id_seq', 1, false);
 
 
 --
--- TOC entry 3175 (class 2606 OID 33267)
--- Name: Account Account_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 3175 (class 2606 OID 28165)
+-- Name: account Account_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Account"
-    ADD CONSTRAINT "Account_pkey" PRIMARY KEY ("ID");
-
-
---
--- TOC entry 3177 (class 2606 OID 33273)
--- Name: Group Group_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."Group"
-    ADD CONSTRAINT "Group_pkey" PRIMARY KEY ("ID");
+ALTER TABLE ONLY public.account
+    ADD CONSTRAINT "Account_pkey" PRIMARY KEY (id);
 
 
 --
--- TOC entry 3180 (class 2606 OID 33291)
--- Name: Account_Group Account_Group_fk0; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 3179 (class 2606 OID 28180)
+-- Name: account_group account_group_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Account_Group"
-    ADD CONSTRAINT "Account_Group_fk0" FOREIGN KEY ("GroupID") REFERENCES public."Group"("ID") NOT VALID;
+ALTER TABLE ONLY public.account_group
+    ADD CONSTRAINT account_group_pkey PRIMARY KEY (id);
 
 
 --
--- TOC entry 3181 (class 2606 OID 33296)
--- Name: Account_Group Account_Group_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 3177 (class 2606 OID 28171)
+-- Name: group group_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Account_Group"
-    ADD CONSTRAINT "Account_Group_fk1" FOREIGN KEY ("AccountID") REFERENCES public."Account"("ID") NOT VALID;
+ALTER TABLE ONLY public."group"
+    ADD CONSTRAINT group_pkey PRIMARY KEY (id);
 
 
--- Completed on 2022-11-14 23:35:03
+--
+-- TOC entry 3180 (class 2606 OID 28181)
+-- Name: account_group account_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.account_group
+    ADD CONSTRAINT account_id FOREIGN KEY (account_id) REFERENCES public.account(id) NOT VALID;
+
+
+--
+-- TOC entry 3181 (class 2606 OID 28186)
+-- Name: account_group group_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.account_group
+    ADD CONSTRAINT group_id FOREIGN KEY (group_id) REFERENCES public."group"(id) NOT VALID;
+
+
+-- Completed on 2022-11-18 21:42:09
 
 --
 -- PostgreSQL database dump complete
 --
-
