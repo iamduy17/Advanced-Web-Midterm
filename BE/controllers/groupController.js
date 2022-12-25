cb23b00af302a03908d5fdb29ad5f9e4dececdd6
DNam018 <duongnamlm10@gmail.com>
2022-12-26 00:34:57 +0700

    update be account_group and group

--- BE/controllers/groupController.js
+++ BE/controllers/groupController.js
@@ -113,6 +113,7 @@ router.post(
     try {
       console.log("remove group with req:", { req });
       const userID = req.user.id;
+      console.log(userID);
       const result = await groupService.RemoveGroup(req.body.group_id, userID);
 
       return res.json(result);
