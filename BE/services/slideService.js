6249fe3ffd44897c224b1b3aaa97e61433025110
unknown <haiduc0147@gmail.com>
2023-01-04 15:02:32 +0700

    chore: remove console log

--- BE/services/slideService.js
+++ BE/services/slideService.js
@@ -47,12 +47,7 @@ const isValidPermission = async (userID, presentationID) => {
       userID,
       presentationID
     );
-  console.log("accountPresentation", accountPresentation);
   if (!accountPresentation || (accountPresentation.role !== ROLE_OWNER && accountPresentation.role !== ROLE_COLLABORATOR)) {
-    console.log(!accountPresentation);
-    console.log(accountPresentation.role !== ROLE_OWNER);
-    console.log(accountPresentation.role !== ROLE_COLLABORATOR);
-    
     return {
       ReturnCode: 401,
       Message: "invalid permission"
